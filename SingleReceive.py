3b93688886b33cd97d1133b956cca7d161c6e618
Steven Burgess <sburgess@dattobackup.com>
2014-03-27 11:12:05 -0400

    first commit of SingleReceive
    This is the single receive test in in its most basic form, just
    receiving the send file and printing out how long it took.

---
+++ SingleReceive.py
@@ -0,0 +1,21 @@
+import time
+import datetime
+import subprocess
+import TestConfig
+import Configs
+import ZfsApi
+
+# Establish where this test will be writing its output
+current_min = time.strftime("%Y%m%d%H%M")
+zfs_receive_path = Configs.test_filesystem_path + '/' + current_min
+
+if ZfsApi.fs_exists(zfs_receive_path):
+    print("Already exists!")
+
+start_time = time.time()
+ZfsApi.zfs_recv(Configs.test_file_full_path, zfs_receive_path)
+end_time = time.time()
+
+time_elapsed = end_time - start_time
+
+print("that took " + str(datetime.timedelta(seconds=time_elapsed)))
