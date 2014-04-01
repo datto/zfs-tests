d1c50343378efa8e44da24784564ff96b233056c
Steven Burgess <sburgess@dattobackup.com>
2014-04-01 14:48:22 -0400

    adds the PID lock
    One of the important things for this test suite is to ensure that only
    one instance of the tests are running at a time.
    
    This will stop people from trying to start 6 instances at once, and
    force them to use the multithreading option.
    
    The advantage of this will be that I can control the creation and
    updating of a stats file knowing that only one process is writing to it,
    and I can handle multithread output in a sane way.

--- SingleReceive.py
+++ SingleReceive.py
@@ -4,6 +4,9 @@ import subprocess
 import TestConfig
 import Configs
 import ZfsApi
+import Pid
+
+Pid.create_pid_file()
 
 # Establish where this test will be writing its output
 current_min = time.strftime("%Y%m%d%H%M")
@@ -19,3 +22,7 @@ end_time = time.time()
 time_elapsed = end_time - start_time
 
 print("that took " + str(datetime.timedelta(seconds=time_elapsed)))
+
+# CLean up the PID file to allow other runs
+Pid.destroy_pid_file()
+
