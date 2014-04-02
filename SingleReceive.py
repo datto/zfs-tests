4bc6085846bfc553ed67025ddd653c48e31d609b
Steven Burgess <sburgess@dattobackup.com>
2014-04-02 15:46:35 -0400

    removes the check for existing filesystems
    Since the PID file is used to ensure that only one run of the ZFS test
    is occurring at a time, we no longer need to check that the zfs
    filesystem is uniquely named.

--- SingleReceive.py
+++ SingleReceive.py
@@ -12,9 +12,6 @@ Pid.create_pid_file()
 current_min = time.strftime("%Y%m%d%H%M%S")
 zfs_receive_path = Configs.test_filesystem_path + '/' + current_min
 
-if ZfsApi.fs_exists(zfs_receive_path):
-    print("Already exists!")
-
 start_time = time.time()
 ZfsApi.zfs_recv(Configs.test_file_full_path, zfs_receive_path)
 end_time = time.time()
