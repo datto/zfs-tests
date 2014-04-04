eff5634ee0f1c6c462f41f7d67867da8216955cb
Steven Burgess <sburgess@dattobackup.com>
2014-04-04 18:11:26 -0400

    adds get_filesystem_properties
    This method gives quick easy access to properties for zfs filesystems.
    It performs a but of checking to ensure that you asked for valid
    properties, then bundles all the requests into a single zfs get. It then
    returns the results a s dict.

--- ZfsApi.py
+++ ZfsApi.py
@@ -16,3 +16,33 @@ def zfs_recv(file_to_receive, desitnation_fs):
     """Receive the specified zfs send file to the destination fs"""
     subprocess.check_call('cat ' + file_to_receive + ' | zfs recv ' + desitnation_fs, shell=True)
 
+def get_filesystem_properties(filesystem_name, property_set):
+    """Given the name of a filesystem and a list of properties returns a
+    dictionary of name value pairs"""
+    # Not all properties are valid filesystem properties, some of them are
+    # snapshot exclusive or the user could mistype. This section checks that
+    # the requested properties exist, and are filesystem properties.
+    file_system_proprties = frozenset(
+            ['available','compressratio','creation','name',
+            'mounted','origin','refcompressratio','referenced','type','used',
+            'usedbychildren','usedbydataset','usedbyrefreservation',
+            'usedbysnapshots','userrefs','written','aclinherit','atime',
+            'canmount','casesensitivity','checksum','compression','copies',
+            'dedup','devices','exec','logbias','mountpoint','normalization',
+            'primarycache','quota','readonly','recordsize','refquota',
+            'refreservation','reservation','secondarycache','setuid',
+            'sharenfs','sharesmb','snapdir','sync','utf8only','version'])
+
+    illegal_properties = frozenset(property_set) - file_system_proprties
+    if illegal_properties:
+        raise ValueError(illegal_properties)
+    # -H strips the header, you do not need it since you know the order
+    # -p gives you the exact values, so time in UNIX timestamps, size in bytes
+    get_response = subprocess.check_output(
+            ['zfs', 'get', '-Hp', ','.join(property_set), filesystem_name])
+    propery_dictionary = {}
+    for line in get_response.splitlines():
+        split_line = line.split()
+        propery_dictionary[split_line[1]] = split_line[2]
+    return propery_dictionary
+
