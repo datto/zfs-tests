8c7792d31719af63f58c2c54fc9ae91465cc8e78
Steven Burgess <sburgess@dattobackup.com>
2014-04-11 17:26:15 -0400

    place the runs in their own filesystem
    This places all runs in their own filesystem so that later we can simply
    destroy the FS. This saves me from having to go through a list of all
    filesystems to determine if they are the runs, the corpus, or the logs.
    It will also make it easier to see space accounting.

--- SingleReceive.py
+++ SingleReceive.py
@@ -11,7 +11,7 @@ Pid.create_pid_file()
 
 # Establish where this test will be writing its output
 current_min = time.strftime("%Y%m%d%H%M%S")
-zfs_receive_path = Configs.test_filesystem_path + '/' + current_min
+zfs_receive_path = Configs.test_filesystem_path + '/runs/' + current_min
 
 start_time = time.time()
 try:
