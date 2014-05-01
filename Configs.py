3fe3dee305dd8c18d73a8577f6a6ce30a48e0724
Steven Burgess <sburgess@dattobackup.com>
2014-05-01 09:45:13 -0400

    Adds log directories
    Adds them to config.cfg and exposes them through Configs.py. These dirs
    will be used to store data about the runs that have occurred.

--- Configs.py
+++ Configs.py
@@ -15,3 +15,12 @@ test_file_full_path = config.get('perf_tests', 'test_file')
 # The directory the tests will occur in.
 mount_point = config.get('perf_tests', 'mount_point')
 
+# The directory to place logs in.
+log_directory = config.get('perf_tests', 'log_directory')
+
+# The directory to put starting and ending results in
+results_directory = config.get('perf_tests', 'results_directory')
+
+# The directory for runnings system statistics during a run
+stats_directory = config.get('perf_tests', 'stats_directory')
+
