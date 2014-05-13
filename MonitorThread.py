6572bfca5875930318982751345ec65c0fc6cb1b
Steven Burgess <sburgess@dattobackup.com>
2014-05-13 15:39:14 -0400

    Use MovingAverage
    This updates MonitorThread to use the MovingAverage class to print out
    better stats for the tracked filesystem

--- MonitorThread.py
+++ MonitorThread.py
@@ -2,6 +2,7 @@ import threading
 import time
 import ZfsApi
 import Common
+import MovingAverage
 '''
 This class will monitor the size of a zfs filesytem, and report the size
 progress over time
@@ -12,20 +13,21 @@ class MonitorThread(threading.Thread):
         threading.Thread.__init__(self)
         self.daemon = True
         self.filesystem = filesystem
+        self.ma = MovingAverage.moving_average([5, 15, 30])
 
     def run(self):
         # Wait for the filesystem that is being monitored to exist.
         while not ZfsApi.fs_exists(self.filesystem):
             time.sleep(1)
-        start_time = time.time()
-        start_size = ZfsApi.get_filesystem_size(self.filesystem)
         while True:
             time.sleep(1)
-            current_size = ZfsApi.get_filesystem_size(self.filesystem)
-            size_diff = current_size - start_size
-            size_diff_in_mib = Common.bytes_to_mebibyte(size_diff)
-            current_time = time.time()
-            time_diff = current_time - start_time
-            mib_per_second = size_diff_in_mib / time_diff
-            print("Average speed: " + str(mib_per_second) + " mib/sec")
+            self.ma.insert_value(ZfsApi.get_filesystem_size(self.filesystem))
+            result_string = ""
+            for size_delta,diff in self.ma.get_diffs():
+                size_diff_in_bytes = float(diff)/size_delta
+                size_diff_in_mib = Common.bytes_to_mebibyte(size_diff_in_bytes)
+                reasonable_string = '%.3f' % size_diff_in_mib
+                result_string = result_string + reasonable_string.rjust(9)
+            print("Average speed in mib/sec")
+            print(result_string)
 
