9d3629773de45b46d4d2cd6d11e3f816af81b255
Steven Burgess <sburgess@dattobackup.com>
2014-05-19 11:23:31 -0400

    Adds a movingaverage for transaction groups
    This second moving average keeps track of the TXGs per second, a metric
    that I want to start tracking.

--- MonitorThread.py
+++ MonitorThread.py
@@ -13,7 +13,9 @@ class MonitorThread(threading.Thread):
         threading.Thread.__init__(self)
         self.daemon = True
         self.filesystem = filesystem
-        self.ma = MovingAverage.moving_average([5, 15, 30])
+        self.size_ma = MovingAverage.moving_average([5, 15, 30])
+        self.poolname = ZfsApi.get_pool_name_for_fs(self.filesystem)
+        self.txg_ma = MovingAverage.moving_average([5, 15, 30])
 
     def run(self):
         # Wait for the filesystem that is being monitored to exist.
@@ -21,9 +23,17 @@ class MonitorThread(threading.Thread):
             time.sleep(1)
         while True:
             time.sleep(1)
-            self.ma.insert_value(ZfsApi.get_filesystem_size(self.filesystem))
+            self.txg_ma.insert_value(ZfsApi.linux_get_current_txg(self.poolname))
+            txg_result_string = ""
+            for txg_delta,diff in self.txg_ma.get_diffs():
+                txg_diff = float(diff)/txg_delta
+                reasonable_string = '%.3f' % txg_diff
+                txg_result_string = txg_result_string + reasonable_string.rjust(9)
+            print("TXGs per second, should be .200")
+            print(txg_result_string)
+            self.size_ma.insert_value(ZfsApi.get_filesystem_size(self.filesystem))
             result_string = ""
-            for size_delta,diff in self.ma.get_diffs():
+            for size_delta,diff in self.size_ma.get_diffs():
                 size_diff_in_bytes = float(diff)/size_delta
                 size_diff_in_mib = Common.bytes_to_mebibyte(size_diff_in_bytes)
                 reasonable_string = '%.3f' % size_diff_in_mib
